8f26d601803d07201ca4f2ae403b6dbdc9b5b376
FranzKrah <f.krah@mailbox.org>
2017-03-14 14:16:58 +0100

    guidance is running
    @heibl im guidance script findest du 2 dinge die du im ips fixen
    könntest

---
+++ R/calc_scores.R
@@ -0,0 +1,40 @@
+#' Wrapper Function of C Code for Calculation of the GUIDANCE Scores
+#'
+#' @param ref of class data.frame, is the reference MSA ('BASE MSA') with sequences as columns
+#' @param com like ref, but 1 alternative ('perturbated MSA')
+#' @param n_id is used as 'i' if function is run in parallel
+#' @return list containing GUIDANCE column score, the residue score and sequence score
+#'
+#' @author Franz-Sebastian Krah
+
+calc_scores <- function(ref, com, n_id = 1){
+
+  locdir <- getwd()
+  locdir_runs <- paste(paste(locdir, "score_run", sep ="/"), n_id, sep ="")
+  dir.create(locdir_runs)
+  seqinr::write.fasta(as.list(ref), names = paste0("S", 1:dim(ref)[1]),
+    file =paste(locdir_runs, "ref.fasta", sep = "/"))
+  seqinr::write.fasta(as.list(com), names = paste0("S", 1:dim(com)[1]),
+    file = paste(locdir_runs, "com.fasta", sep = "/"))
+
+
+  system(paste(paste(locdir, "src/msa_set_score", sep ="/"),
+    paste(locdir_runs, "ref.fasta", sep="/"),
+    paste(paste("score_run", n_id, sep =""), "score_res", sep="/"),
+    "-m",
+    paste(paste("score_run", n_id, sep =""), "com.fasta", sep="/")))
+
+  files <- list.files(locdir_runs)
+  read <- files[grep("score", files)]
+  # GUIDANCE Score
+  gsc <- read.table(paste(locdir_runs, read[3], sep ="/"), header = FALSE)
+  # G residue score
+  grsc <- read.table(paste(locdir_runs, read[4], sep ="/"), header = FALSE)
+  # sequence score
+  ssc <- read.table(paste(locdir_runs, read[6], sep ="/"), header = FALSE)
+
+  unlink(locdir_runs, recursive = TRUE, force = TRUE)
+
+  res <- list(GUIDANCE_score = gsc, residue_score = grsc, sequence_score = ssc)
+  return(res)
+}
--- dev/00.R
+++ dev/00.R
@@ -1,31 +1,28 @@
-library(ips); library(foreach);
-library(parallel)
-library(doSNOW)
+library("ips")
+library("foreach")
+library("parallel")
+library("doSNOW")
 library(pbmcapply)
-
 # 1.	Read sequences
 ms <- read.fas("dev/data/cortinarius_28s_ms.fas")
 # ms <- read.FASTA("data/_28s.fas")
 set.seed(100)
 ms_test <- sample(ms, 10)
-# seqinr::write.fasta(as.character(ms_test), names = names(ms_test),
-#   file = "dev/out/ms_test.fas")
-
-# ms_test <- read.FASTA("out/msa_test.fas")
 
 
 dna = ms_test
-ncore = 24
+ncore = 2
 bootstrap = 100
-msa.programm = "mafft"
+msa.program = "mafft"
 method = "auto"
 parallel  = TRUE
-G.CS.th = 0.93
+cutoff = 0.93
 
-# system.time(test <- guidance(dna = ms_test,
-#                              ncore = 12,
-#                              bootstrap = 100,
-#                              msa.programm = "mafft",
-#                              parallel  = TRUE,
-#                              G.CS.th = 0.93))
-#
+system.time(test <- guidance(dna = ms_test,
+                                ncore = 2,
+                                bootstrap = 100,
+                                msa.program = "mafft",
+                                method = "auto",
+                                parallel  = TRUE,
+                                cutoff = 0.93))
+# 15 min with 2 cores
